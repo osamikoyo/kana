562db98d7508b76a7c2b59088acf1ac8cd720fcd
osamikoyo <v.pylayev@mail.ru>
2025-01-31 04:47:07 +0500

    first

---
+++ src/config.c
@@ -0,0 +1,10 @@
+//
+// Created by osami on 31.01.25.
+//
+
+#include "config.h"
+#include "../lib/toml/toml.h"
+
+void get_config_result(char **path_variable, char *filepath) {
+
+}
---
+++ src/config.h
@@ -0,0 +1,11 @@
+//
+// Created by osami on 31.01.25.
+//
+
+#ifndef CONFIG_H
+
+void get_config_values(const char *)
+
+#define CONFIG_H
+
+#endif //CONFIG_H
